0c96d413cf7322b76739270ba4c7b24629da3271
crussong <mike1lusson@gmail.com>
2026-02-03 18:36:01 +0100

    Add files via upload

--- assets/js/layout-unified.js
+++ assets/js/layout-unified.js
@@ -508,6 +508,9 @@ function createUnifiedDock() {
             </div>
         </a>
         
+        <!-- Spacer between Character and Session Card -->
+        <div class="dock__card-spacer"></div>
+        
         <!-- Session Card (loaded dynamically) -->
         <a href="session.html" class="dock__session-card hidden" id="dockSessionCard">
             <div class="dock__session-portrait" id="dockSessionPortrait">
@@ -516,15 +519,6 @@ function createUnifiedDock() {
                 </div>
             </div>
         </a>
-        
-        ${(userData.isGM || userData.isCogm) ? `
-        <div class="dock__divider"></div>
-        <a href="gm.html" class="dock__item dock__item--gm ${currentPage === 'gm' ? 'active' : ''}" data-tooltip="GM Optionen">
-            <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M11.884 2.007l.114 -.007l.118 .007l.059 .008l.061 .013l.111 .034a.993 .993 0 0 1 .217 .112l.104 .082l.255 .218a11 11 0 0 0 7.189 2.537l.342 -.01a1 1 0 0 1 1.005 .717a13 13 0 0 1 -9.208 16.25a1 1 0 0 1 -.502 0a13 13 0 0 1 -9.209 -16.25a1 1 0 0 1 1.005 -.717a11 11 0 0 0 7.531 -2.527l.263 -.225l.096 -.075a.993 .993 0 0 1 .217 -.112l.112 -.034a.97 .97 0 0 1 .119 -.021z"/>
-            </svg>
-        </a>
-        ` : ''}
     </div>
     `;
 }
